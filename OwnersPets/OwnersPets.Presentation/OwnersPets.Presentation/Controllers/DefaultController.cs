16f6d9fef81762618a4243cc4320cbf153cc3455
Ivan Vertegel <sirius9764@gmail.com>
2017-04-13 23:50:54 +0300

    Index markup has been created

---
+++ OwnersPets/OwnersPets.Presentation/OwnersPets.Presentation/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace OwnersPets.Presentation.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
