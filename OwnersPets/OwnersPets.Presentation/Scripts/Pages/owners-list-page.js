b938ee9f29742244deeef9fa41fe6eb034dd4a7a
Ivan Vertegel <sirius9764@gmail.com>
2017-04-14 23:15:24 +0300

    Added server communication, angular, pagination

---
+++ OwnersPets/OwnersPets.Presentation/Scripts/Pages/owners-list-page.js
@@ -0,0 +1,45 @@
+﻿angular.module('OwnersPage',  ['ngSanitize'])
+.controller('OwnersPageController', ['$scope', '$http', function ($scope, $http) {
+    var defaultItemsCountOnPage = 3;
+    $scope.currentPage = 0;
+
+    $scope.paginationItem_OnClick = function(pageNumber) {
+       $scope.currentPage = pageNumber-1;
+    };
+
+    var getOwnersList = function () {
+        $http({
+            method: 'GET',
+            url: 'api/Owners'
+        }).then(function (response) {
+            return response.data;
+        }).then(function(response) {
+            $scope.OwnersList = response;
+            $scope.Pages = splitOwnersListToPages(response);
+        });
+    };
+
+    var splitOwnersListToPages = function(ownersList) {
+        // pages generation
+        var result = [];
+        for (var i=0, pageNumber = 1; i<ownersList.length; i+=defaultItemsCountOnPage, pageNumber++)
+        {
+            var itemsOnPage = [];
+            for(var j=i; j<i+defaultItemsCountOnPage && j<ownersList.length; j++)
+            {
+                itemsOnPage.push(ownersList[j]);
+            }
+
+            var page = {
+                Number : pageNumber,
+                Items : itemsOnPage
+            };
+
+            result.push(page);
+        }
+
+        return result;
+    };
+
+    getOwnersList();
+}]);
--- OwnersPets/OwnersPets/Service/OwnersService.cs
+++ OwnersPets/OwnersPets/Service/OwnersService.cs
@@ -12,7 +12,14 @@ namespace OwnersPets.Service
             {
                 return new Owner[]
                 {
-                    new Owner { Name = "Bob", PetsCount = 10 }
+                    new Owner { Name = "Bob", PetsCount = 10 },
+                    new Owner { Name = "Simpsons", PetsCount = 20 },
+                    new Owner { Name = "Michael", PetsCount = 30 },
+                    new Owner { Name = "Mark", PetsCount = 3 },
+                    new Owner { Name = "Andrew", PetsCount = 12 },
+                    new Owner { Name = "Thomas", PetsCount = 15 },
+                    new Owner { Name = "Martin", PetsCount = 5 },
+                    new Owner { Name = "Batman", PetsCount = 7 },
                 };
             });
         }
